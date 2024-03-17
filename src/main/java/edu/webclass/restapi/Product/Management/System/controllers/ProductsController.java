4d085c8b1791156b5f65091c5d25cc5500a95142
Mohammad <zare.6984@gmail.com>
2024-03-18 01:30:12 +0330

    Initial commit

---
+++ src/main/java/edu/webclass/restapi/Product/Management/System/controllers/ProductsController.java
@@ -0,0 +1,43 @@
+package edu.webclass.restapi.Product.Management.System.controllers;
+
+import edu.webclass.restapi.Product.Management.System.models.Product;
+import edu.webclass.restapi.Product.Management.System.models.dto.ProductDto;
+import edu.webclass.restapi.Product.Management.System.services.ProductService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
+
+import java.util.List;
+
+import static org.springframework.http.HttpStatus.NOT_FOUND;
+
+@RestController
+@RequestMapping("/api/v1/products")
+public class ProductsController {
+
+    @Autowired
+    private ProductService productService;
+
+    @GetMapping("/list")
+    public List<ProductDto> listAllProducts(){
+        return productService.findAllProducts().stream().map(product -> new ProductDto(product)).toList();
+    }
+
+    @PostMapping("/add")
+    public boolean addProduct(@RequestHeader("name") String title,@RequestHeader String brand,@RequestHeader int price){
+        return productService.addProduct(title,brand,price);
+    }
+    @GetMapping("/{id}")
+    public ResponseEntity<ProductDto> getProductById(@PathVariable("id") String id){
+        Product product = productService.getProductById(id);
+        if(product == null){
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(null);
+        } else {
+            return ResponseEntity.ok(new ProductDto(product));
+        }
+    }
+
+
+}
---
+++ src/main/java/edu/webclass/restapi/Product/Management/System/repository/ProductRepository.java
@@ -0,0 +1,43 @@
+package edu.webclass.restapi.Product.Management.System.repository;
+
+import edu.webclass.restapi.Product.Management.System.models.Product;
+import org.springframework.stereotype.Repository;
+import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.springframework.http.HttpStatus.NOT_FOUND;
+
+@Service
+public class ProductRepository {
+    public static ArrayList<Product> products= new ArrayList<>();
+    public static int lastIndex=0;
+
+    public ProductRepository(){
+        products.add(new Product("Mobile","Samsung", 50000000));
+        products.add(new Product("Cacke","TITOP!",10000));
+    }
+
+    public boolean createNewProduct(Product product){
+        products.add(product);
+        if(ProductRepository.lastIndex == ProductRepository.products.size()){
+            return true;
+        }else{
+            return false;
+        }
+    }
+
+    public List<Product> findAllProducts(){
+        return products;
+    }
+
+    public Product getProductByName(String id){
+        for (Product x : products) {
+            if(x.id.equals(id))
+                return x;
+        }
+        return null;
+    }
+}
